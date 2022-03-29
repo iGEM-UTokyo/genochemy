a74638c1d9e6e2a4a49d54499251ccee41b40f7a
shundroid <shundroid@users.noreply.github.com>
2022-03-29 12:03:28 +0900

    tray

---
+++ components/Tray.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tray">
+    <BlocksPromoter />
+    <BlocksVisible />
+  </div>
+</template>
+
+<style scoped>
+.tray {
+  width: 30%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding-right: 20px;
+  border-right: 1px solid #aaa;
+}
+</style>
---
+++ components/blocks/Visible.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img src="/blocks/visible.svg" />
+    <span>mCherry</span>
+  </div>
+</template>
+
+<style scoped>
+div {
+  position: relative;
+}
+img {
+  width: 100%;
+}
+span {
+  color: white;
+  position: absolute;
+  left: 20px;
+  bottom: 15px;
+  font-size: 18px;
+}
+</style>
