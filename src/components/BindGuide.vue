41d49385167467d0025d234d8425b6651e49cab6
shundroid <shundroid@users.noreply.github.com>
2022-04-01 18:40:19 +0900

    bindguide

---
+++ src/components/BindGuide.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :style="style" class="bind-guide">
+    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+      <line x1="0" y1="0" x2="100" y2="100" stroke="black" stroke-width="2" />
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Vector2 } from '@/utils/block';
+import { StyleValue, defineProps, ComputedRef, computed } from 'vue';
+
+const props = defineProps<{
+  positions: [Vector2, Vector2]
+}>()
+
+const style: ComputedRef<StyleValue> = computed(() => {
+  const widthBase = props.positions[0][0] - props.positions[1][0]
+  const heightBase = props.positions[0][1] - props.positions[1][1]
+  const result: StyleValue = {
+    top: `${Math.min(props.positions[0][1], props.positions[1][1]) - 15}px`,
+    left: `${Math.min(props.positions[0][0], props.positions[1][0])}px`,
+    width: `${Math.abs(widthBase)}px`,
+    height: `${Math.abs(heightBase)}px`,
+  }
+  if (widthBase * heightBase < 0) {
+    result['transform'] = 'scaleX(-1)'
+  }
+  return result
+})
+
+</script>
+
+<style scoped>
+.bind-guide {
+  position: absolute;
+  display: flex;
+}
+.bind-guide svg {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/components/Snake.vue
+++ src/components/Snake.vue
@@ -4,6 +4,11 @@
       v-for="block in props.snake.blocks"
       :key="block.uuid"
       :block="block" />
+    <teleport to=".program">
+      <BindGuide
+        v-if="currentBindGuide !== null"
+        :positions="currentBindGuide" />
+    </teleport>
   </div>
 </template>
 
@@ -11,39 +16,78 @@
 import { Snake } from '@/utils/snake'
 import { useStore } from '../store'
 import Block from '@/components/Block.vue'
+import BindGuide from '@/components/BindGuide.vue'
 import { Ref, ref, defineProps, computed, StyleValue, ComputedRef } from 'vue';
+import { Vector2 } from '@/utils/block';
 
 const props = defineProps<{
   snake: Snake
 }>()
 
 const snakeRef: Ref<HTMLElement | null> = ref(null)
-const { updateSnake } = useStore()
+const { updateSnake, snakes } = useStore()
+// non-reactive. updated on mousedown
+let prevAnchors: { pos: Vector2, uuid: string }[] = []
+// non-reactive. updated on mousedown
+let nextAnchors: { pos: Vector2, uuid: string }[] = []
 const down = () => {
   window.addEventListener('mousemove', move)
+  prevAnchors = []
+  nextAnchors = []
+  for (let snake of Object.values(snakes)) {
+    prevAnchors.push({ pos: snake.anchorPrev, uuid: snake.uuid })
+    nextAnchors.push({ pos: snake.anchorNext, uuid: snake.uuid })
+  }
 }
 
-const bottom = ref(0)
-const left = ref(0)
+const anchorPrev = computed(() => props.snake.anchorPrev)
 const style: ComputedRef<StyleValue> = computed(() => ({
   position: 'absolute',
-  top: `${bottom.value - (snakeRef.value ? snakeRef.value.clientHeight : 0)}px`,
-  left: `${left.value}px`,
+  top: `${anchorPrev.value[1] - (snakeRef.value ? snakeRef.value.clientHeight : 0)}px`,
+  left: `${anchorPrev.value[0]}px`,
 }))
-// eslint-disable-next-line vue/no-setup-props-destructure
-bottom.value = props.snake.anchorPrev[1]
-// eslint-disable-next-line vue/no-setup-props-destructure
-left.value = props.snake.anchorPrev[0]
 
+let currentBindUUID: string | null = null
+const currentBindGuide: Ref<[Vector2, Vector2] | null> = ref(null)
 const move = (event: MouseEvent) => {
-  bottom.value += event.movementY
-  left.value += event.movementX
+  // eslint-disable-next-line vue/no-mutating-props
+  props.snake.anchorPrev[0] += event.movementX
+  // eslint-disable-next-line vue/no-mutating-props
+  props.snake.anchorPrev[1] += event.movementY
+  let hasSet = false
+  const _next = props.snake.anchorNext
+  for (let prevAnchor of prevAnchors) {
+    if (prevAnchor.uuid === props.snake.uuid) continue
+    const distance = (prevAnchor.pos[0] - _next[0]) ** 2 + (prevAnchor.pos[1] - _next[1]) ** 2
+    if (distance <= 50 ** 2) {
+      currentBindUUID = prevAnchor.uuid
+      currentBindGuide.value = [prevAnchor.pos, _next]
+      hasSet = true
+    }
+  }
+  const _prev = anchorPrev.value
+  for (let nextAnchor of nextAnchors) {
+    if (nextAnchor.uuid === props.snake.uuid) continue
+    const distance = (nextAnchor.pos[0] - _prev[0]) ** 2 + (nextAnchor.pos[1] - _prev[1]) ** 2
+    if (distance <= 50 ** 2) {
+      currentBindUUID = nextAnchor.uuid
+      currentBindGuide.value = [nextAnchor.pos, _prev]
+      hasSet = true
+    }
+  }
+  if (!hasSet) {
+    currentBindUUID = null
+    currentBindGuide.value = null
+  }
 }
-window.addEventListener('mousemove', move)
 const up = () => {
   window.removeEventListener('mousemove', move)
+  currentBindGuide.value = null
   const newSnake = Object.assign(Object.create(Object.getPrototypeOf(props.snake)), props.snake) as Snake
-  newSnake.anchorPrev = [left.value, bottom.value]
+  newSnake.anchorPrev = anchorPrev.value
   updateSnake(newSnake)
 }
+
+// on setup, mouse is down
+down()
 </script>
