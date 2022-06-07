062189027d98f68d89063b83dece71e42fc8f9de
shundroid <shundroid@users.noreply.github.com>
2022-06-07 12:19:55 +0900

    implemented runner output

--- src/components/Runner.vue
+++ src/components/Runner.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="runner">
     <img src="/runner/bacterium.svg" class="bacterium" />
-    <div class="light" />
+    <div class="light" :style="{ backgroundColor: lightRGBA }" />
   </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useStore } from "../store";
+const { runnerOutputs } = useStore();
+
+const mCherryColor = [208, 25, 187];
+const lightRGBA = computed(() => `rgba(${mCherryColor[0]}, ${mCherryColor[1]}, ${mCherryColor[2]}, ${Math.min(runnerOutputs.lightEmission * 0.8, 0.8)})`)
+</script>
 
 <style scoped>
 .runner {
@@ -22,8 +31,8 @@
   width: 50px;
   height: 100px;
   top: 60px;
-  background-color: red;
-  opacity: 0.5;
+  /* background-color: red;
+  opacity: 0.5; */
   border-radius: 50px;
 }
 </style>
