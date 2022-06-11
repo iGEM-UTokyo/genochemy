058dae981876585fbfd953f4b6c83d9a19d56a7a
shundroid <shundroid@users.noreply.github.com>
2022-06-11 13:53:21 +0900

    supported program scroll

--- src/components/Program.vue
+++ src/components/Program.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="program" ref="programRef">
-    <Snake v-for="snake in snakes" :key="snake.uuid" :snake="snake" />
+    <div
+      class="inner-program"
+      :style="{ width: `${size[0]}px`, height: `${size[1]}px` }"
+    >
+      <Snake v-for="snake in snakes" :key="snake.uuid" :snake="snake" />
+    </div>
   </div>
 </template>
 
@@ -14,7 +19,7 @@ export const getAbsolutePositionKey: InjectionKey<
 </script>
 
 <script setup lang="ts">
-import { InjectionKey, provide, Ref, ref } from "vue";
+import { InjectionKey, provide, Ref, ref, computed } from "vue";
 import { useStore } from "../store";
 import Snake from "@/components/Snake.vue";
 import { Vector2 } from "@/utils/block";
@@ -23,12 +28,25 @@ const { snakes } = useStore();
 
 const programRef: Ref<HTMLElement | null> = ref(null);
 
+const size = computed(() => {
+  return Object.values(snakes).reduce(
+    (a, b) => [
+      Math.max(a[0], b.anchorNext[0]),
+      Math.max(a[1], b.anchorNext[1]),
+    ],
+    [0, 0]
+  );
+});
+
 provide(getFixedPositionKey, (absolutePos: Vector2) => {
   if (programRef.value === null) {
     throw new Error("programRef is null.");
   }
   const boundingRect = programRef.value.getBoundingClientRect();
-  return [absolutePos[0] + boundingRect.x, absolutePos[1] + boundingRect.y];
+  return [
+    absolutePos[0] + boundingRect.x - programRef.value.scrollLeft,
+    absolutePos[1] + boundingRect.y - programRef.value.scrollTop,
+  ];
 });
 
 provide(getAbsolutePositionKey, (fixedPos: Vector2) => {
