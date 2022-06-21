d3629b4f8f05a374e1b6571606c10f7e4f476688
shundroid <shundroid@users.noreply.github.com>
2022-06-21 16:22:09 +0900

    fixed fluorescence

---
+++ src/components/on-runner/Fluorescence.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="light" :style="{ backgroundColor: lightRGBA }" />
+</template>
+
+<script lang="ts">
+export default {
+  name: "Fluorescence",
+  customOptions: {},
+};
+</script>
+
+<script setup lang="ts">
+import { computed, onUnmounted, toRefs } from "vue";
+import { useStore } from "@/store";
+
+const { runnerOutputs, registerOutput, UnregisterOutput } = useStore();
+
+type Color = [number, number, number];
+const outputNames = ["protein-GFP", "protein-mCherry"] as const;
+for (const name of outputNames) {
+  registerOutput(name);
+}
+onUnmounted(() => {
+  for (const name of outputNames) {
+    UnregisterOutput(name);
+  }
+});
+
+const colors: { [key in typeof outputNames[number]]: Color } = {
+  "protein-GFP": [0, 255, 0],
+  "protein-mCherry": [255, 9, 91],
+};
+const lightRGBA = computed(() => {
+  const color = [0, 0, 0];
+  let sum = 0;
+  let max = 0;
+  for (const name of outputNames) {
+    sum += runnerOutputs[name];
+    max = Math.max(max, runnerOutputs[name]);
+    for (let i = 0; i < 3; i++) {
+      color[i] += colors[name][i] * runnerOutputs[name];
+    }
+  }
+  if (sum > 0) {
+    for (let i = 0; i < 3; i++) {
+      color[i] /= sum;
+    }
+  }
+  const opacity = max / (0.5 + max);
+  return `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${opacity})`;
+});
+</script>
+
+<style scoped>
+.light {
+  position: absolute;
+  width: 37px;
+  height: 88px;
+  top: 68px;
+  left: 230px;
+  /* background-color: red;
+  opacity: 0.5; */
+  border-radius: 50px;
+}
+</style>
