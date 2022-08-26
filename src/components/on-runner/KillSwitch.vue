c57ee184b45f049d8cd8e73df8953be27035cb7c
shundroid <shundroid@users.noreply.github.com>
2022-08-26 22:41:51 +0900

    fixed recombinase bug

--- src/components/on-runner/KillSwitch.vue
+++ src/components/on-runner/KillSwitch.vue
@@ -18,7 +18,8 @@ const { runnerOutputs, registerOutput, UnregisterOutput, kill } = store;
 
 const { isRunning, currentRunner } = toRefs(store);
 watch(currentRunner, () => {
-  pCount = 0;
+  convergeCount = 0;
+  prevOutput = 0;
 });
 
 registerOutput("protein-KillSwitch");
@@ -27,19 +28,22 @@ onUnmounted(() => {
 });
 
 let requestAnimationFrameId: number | null = null;
-const pDetermineThreshold = 0.0065;
-let pCount = 0;
+let convergeCount = 0;
+let prevOutput = 0;
 function tick() {
   requestAnimationFrameId = null;
   if (!isRunning.value) return;
   const p = 1 - Math.exp(-runnerOutputs["protein-KillSwitch"] * 0.007);
-  const rand = Math.random();
-  if (rand <= p || pCount === Math.floor(1 / pDetermineThreshold)) {
-    kill();
+  if (p >= 0.001) {
+    const rand = Math.random();
+    if (rand <= p || convergeCount === 50) {
+      kill();
+    }
+    if (Math.abs(runnerOutputs["protein-KillSwitch"] - prevOutput) <= 0.0001) {
+      convergeCount++;
+    }
   }
-  if (p >= pDetermineThreshold) {
-    pCount++;
-  }
+  prevOutput = runnerOutputs["protein-KillSwitch"];
   requestAnimationFrameId = requestAnimationFrame(tick);
 }
 
