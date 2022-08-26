df4f09fff43ec749cad0ed67fd9ef24a4c089d86
shundroid <shundroid@users.noreply.github.com>
2022-08-26 22:41:51 +0900

    fixed recombinase bug

--- src/components/on-runner/RecombinaseModifier.vue
+++ src/components/on-runner/RecombinaseModifier.vue
@@ -40,7 +40,11 @@ const recombinases = [
 
 const { isRunning, snakes, currentRunner } = toRefs(store);
 watch(currentRunner, () => {
-  p2Count = 0;
+  prevOutputs = {};
+  for (const recombinase of recombinases) {
+    prevOutputs[recombinase.name] = 0;
+    convergeCount[recombinase.name] = 0;
+  }
 });
 
 for (const recombinase of recombinases) {
@@ -53,96 +57,106 @@ onUnmounted(() => {
 });
 
 let requestAnimationFrameId: number | null = null;
-const p2DetermineThreshold = 0.0065;
-let p2Count = 0;
+let convergeCount: Record<string, number> = {};
+let prevOutputs: Record<string, number> = {};
 function tick() {
   requestAnimationFrameId = null;
   if (!isRunning.value) return;
   for (const recombinase of recombinases) {
     const p1 = 1 - Math.exp(-runnerOutputs[recombinase.name] * 0.002);
     const p2 = 1 - Math.exp(-runnerOutputs[recombinase.name] * 0.007);
-    const rand = Math.random();
-    if (rand <= p1) {
-      const recogSeqUUIDs: {
-        snakeUUID: string;
-        index: number;
-      }[] = [];
-      for (const snake of Object.values(snakes.value)) {
-        if (snake.isLoop) continue;
-        for (let i = 0; i < snake.blocks.length; i++) {
-          if (snake.blocks[i] instanceof recombinase.recogSeqBlock) {
-            recogSeqUUIDs.push({
-              snakeUUID: snake.uuid,
-              index: i,
-            });
-          }
-        }
-      }
-      if (recogSeqUUIDs.length >= 2) {
-        const firstRecogSeq = recogSeqUUIDs.splice(
-          Math.floor(Math.random() * recogSeqUUIDs.length),
-          1
-        )[0];
-        const secondRecogSeq = recogSeqUUIDs.splice(
-          Math.floor(Math.random() * recogSeqUUIDs.length),
-          1
-        )[0];
-        if (firstRecogSeq.snakeUUID === secondRecogSeq.snakeUUID) {
-          cutSnake(
-            firstRecogSeq.snakeUUID,
-            Math.min(firstRecogSeq.index, secondRecogSeq.index),
-            Math.max(firstRecogSeq.index, secondRecogSeq.index)
-          );
-        } else {
-          recombineTwoSnakes(
-            firstRecogSeq.snakeUUID,
-            secondRecogSeq.snakeUUID,
-            firstRecogSeq.index,
-            secondRecogSeq.index
-          );
-        }
-        updateRunner();
-      }
-    } else if (rand <= p2 || p2Count === Math.floor(1 / p2DetermineThreshold)) {
-      const recogSeqUUIDs: { [snakeUUID: string]: number[] } = {};
-      for (const snake of Object.values(snakes.value)) {
-        if (snake.isLoop) continue;
-        for (let i = 0; i < snake.blocks.length; i++) {
-          if (snake.blocks[i] instanceof recombinase.recogSeqBlock) {
-            if (!recogSeqUUIDs[snake.uuid]) {
-              recogSeqUUIDs[snake.uuid] = [];
+    if (p2 > 0.001) {
+      const rand = Math.random();
+      if (rand <= p1) {
+        const recogSeqUUIDs: {
+          snakeUUID: string;
+          index: number;
+        }[] = [];
+        for (const snake of Object.values(snakes.value)) {
+          if (snake.isLoop) continue;
+          for (let i = 0; i < snake.blocks.length; i++) {
+            if (snake.blocks[i] instanceof recombinase.recogSeqBlock) {
+              recogSeqUUIDs.push({
+                snakeUUID: snake.uuid,
+                index: i,
+              });
             }
-            recogSeqUUIDs[snake.uuid].push(i);
           }
         }
-        if (recogSeqUUIDs[snake.uuid] && recogSeqUUIDs[snake.uuid].length < 2) {
-          delete recogSeqUUIDs[snake.uuid];
+        if (recogSeqUUIDs.length >= 2) {
+          const firstRecogSeq = recogSeqUUIDs.splice(
+            Math.floor(Math.random() * recogSeqUUIDs.length),
+            1
+          )[0];
+          const secondRecogSeq = recogSeqUUIDs.splice(
+            Math.floor(Math.random() * recogSeqUUIDs.length),
+            1
+          )[0];
+          if (firstRecogSeq.snakeUUID === secondRecogSeq.snakeUUID) {
+            cutSnake(
+              firstRecogSeq.snakeUUID,
+              Math.min(firstRecogSeq.index, secondRecogSeq.index),
+              Math.max(firstRecogSeq.index, secondRecogSeq.index)
+            );
+          } else {
+            recombineTwoSnakes(
+              firstRecogSeq.snakeUUID,
+              secondRecogSeq.snakeUUID,
+              firstRecogSeq.index,
+              secondRecogSeq.index
+            );
+          }
+          updateRunner();
+        }
+      } else if (rand <= p2 || convergeCount[recombinase.name] === 50) {
+        const recogSeqUUIDs: { [snakeUUID: string]: number[] } = {};
+        for (const snake of Object.values(snakes.value)) {
+          if (snake.isLoop) continue;
+          for (let i = 0; i < snake.blocks.length; i++) {
+            if (snake.blocks[i] instanceof recombinase.recogSeqBlock) {
+              if (!recogSeqUUIDs[snake.uuid]) {
+                recogSeqUUIDs[snake.uuid] = [];
+              }
+              recogSeqUUIDs[snake.uuid].push(i);
+            }
+          }
+          if (
+            recogSeqUUIDs[snake.uuid] &&
+            recogSeqUUIDs[snake.uuid].length < 2
+          ) {
+            delete recogSeqUUIDs[snake.uuid];
+          }
+        }
+        const snakeUUIDs = Object.keys(recogSeqUUIDs);
+        if (snakeUUIDs.length !== 0) {
+          const targetSnakeUUID =
+            snakeUUIDs[Math.floor(Math.random() * snakeUUIDs.length)];
+          const targetSnakeIndices = recogSeqUUIDs[targetSnakeUUID];
+          const firstRecogSeq = targetSnakeIndices.splice(
+            Math.floor(Math.random() * targetSnakeIndices.length),
+            1
+          )[0];
+          const secondRecogSeq = targetSnakeIndices.splice(
+            Math.floor(Math.random() * targetSnakeIndices.length),
+            1
+          )[0];
+          cutSnake(
+            targetSnakeUUID,
+            Math.min(firstRecogSeq, secondRecogSeq),
+            Math.max(firstRecogSeq, secondRecogSeq)
+          );
+          updateRunner();
         }
       }
-      const snakeUUIDs = Object.keys(recogSeqUUIDs);
-      if (snakeUUIDs.length !== 0) {
-        const targetSnakeUUID =
-          snakeUUIDs[Math.floor(Math.random() * snakeUUIDs.length)];
-        const targetSnakeIndices = recogSeqUUIDs[targetSnakeUUID];
-        const firstRecogSeq = targetSnakeIndices.splice(
-          Math.floor(Math.random() * targetSnakeIndices.length),
-          1
-        )[0];
-        const secondRecogSeq = targetSnakeIndices.splice(
-          Math.floor(Math.random() * targetSnakeIndices.length),
-          1
-        )[0];
-        cutSnake(
-          targetSnakeUUID,
-          Math.min(firstRecogSeq, secondRecogSeq),
-          Math.max(firstRecogSeq, secondRecogSeq)
-        );
-        updateRunner();
+      if (
+        Math.abs(
+          runnerOutputs[recombinase.name] - prevOutputs[recombinase.name]
+        ) <= 0.0001
+      ) {
+        convergeCount[recombinase.name]++;
       }
     }
-    if (p2 >= p2DetermineThreshold) {
-      p2Count++;
-    }
+    prevOutputs[recombinase.name] = runnerOutputs[recombinase.name];
   }
   requestAnimationFrameId = requestAnimationFrame(tick);
 }
