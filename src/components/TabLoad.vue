972bb584a0f728a017b31134153c849e08dc2129
shundroid <shundroid@users.noreply.github.com>
2022-09-08 19:38:03 +0900

    can import seq

--- src/components/TabLoad.vue
+++ src/components/TabLoad.vue
@@ -6,66 +6,14 @@
 
 <script setup lang="ts">
 import { useStore } from "@/store";
-import * as blocks from "@/utils/block";
-import { Snake } from "@/utils/snake";
-import { v4 as uuidv4 } from "uuid";
+import questions from "@/utils/questions";
+import importJson from "@/utils/importer";
 
 const { addSnake } = useStore();
 
-function bulkSetUUID(
-  blocks: blocks.Block[]
-): asserts blocks is blocks.BlockWithUUID[] {
-  for (const block of blocks) {
-    block.uuid = uuidv4();
-  }
-}
 function load() {
-  const blocks1 = [
-    new blocks.T7PromoterBlock(),
-    new blocks.EL222Block(),
-    new blocks.PhyBBlock(),
-    new blocks.PIF3Block(),
-    new blocks.CYC1TerminatorBlock(),
-  ];
-  bulkSetUUID(blocks1);
-  const snake1 = new Snake({
-    uuid: uuidv4(),
-    blocks: blocks1,
-    anchorTail: [10, 100],
-  });
-  addSnake(snake1);
-  const blocks2 = [
-    new blocks.EL222ActivatedPromoterBlock(),
-    new blocks.RecombinaseBRecognitionSeqBlock(),
-    new blocks.RecombinaseABlock(),
-    new blocks.CYC1TerminatorBlock(),
-    new blocks.RecombinaseBRecognitionSeqBlock(),
-    new blocks.GFPBlock(),
-    new blocks.CYC1TerminatorBlock(),
-  ];
-  bulkSetUUID(blocks2);
-  const snake2 = new Snake({
-    uuid: uuidv4(),
-    blocks: blocks2,
-    anchorTail: [10, 200],
-  });
-  addSnake(snake2);
-  const blocks3 = [
-    new blocks.PhyBPIF3ActivatedPromoterBlock(),
-    new blocks.RecombinaseARecognitionSeqBlock(),
-    new blocks.KillSwitchBlock(),
-    new blocks.CYC1TerminatorBlock(),
-    new blocks.RecombinaseARecognitionSeqBlock(),
-    new blocks.RecombinaseBBlock(),
-    new blocks.CYC1TerminatorBlock(),
-  ];
-  bulkSetUUID(blocks3);
-  const snake3 = new Snake({
-    uuid: uuidv4(),
-    blocks: blocks3,
-    anchorTail: [10, 300],
-  });
-  addSnake(snake3);
+  const snakes = importJson(questions[7].answer);
+  addSnake(...snakes);
 }
 </script>
 
--- src/utils/questions.ts
+++ src/utils/questions.ts
@@ -1,40 +1,83 @@
+import { JsonFormat } from "./importer";
+
 interface Question {
   name: string;
   imgs: string[];
+  answer: JsonFormat;
 }
 
 const questions: Question[] = [
   {
     name: "Green Creature",
     imgs: ["Question1.png"],
+    answer: [],
   },
   {
     name: "Blue makes green",
     imgs: ["Question2.png"],
+    answer: [],
   },
   {
     name: "Killer Light",
     imgs: ["Question2.5.png"],
+    answer: [],
   },
   {
     name: "Drug Addict",
     imgs: ["Question3.png"],
+    answer: [],
   },
   {
     name: "NAND Gate",
     imgs: ["Question4-1.png", "Question4-2.png"],
+    answer: [],
   },
   {
     name: "Short Temper",
     imgs: ["Question5.png"],
+    answer: [],
   },
   {
     name: "Blue makes infinitely green",
     imgs: ["Question6.png"],
+    answer: [],
   },
   {
     name: "Optopass",
     imgs: ["Question7.png"],
+    answer: [
+      {
+        blocks: [
+          "prom-const-1",
+          "ctrl-EL222",
+          "ctrl-PhyB",
+          "ctrl-PIF3",
+          "term-1",
+        ],
+      },
+      {
+        blocks: [
+          "prom-activ-EL222dim",
+          "seq-recog-recomb2",
+          "meta-recomb1",
+          "term-1",
+          "seq-recog-recomb2",
+          "visi-GFP",
+          "term-1",
+        ],
+      },
+      {
+        blocks: [
+          "prom-activ-PhyBPIF3",
+          "seq-recog-recomb1",
+          "meta-kill",
+          "term-1",
+          "seq-recog-recomb1",
+          "meta-recomb2",
+          "term-1",
+        ],
+      },
+    ],
   },
 ];
 
