8ba39410a9f7ae7f57a41e640670f78df444825c
shundroid <shundroid@users.noreply.github.com>
2022-09-14 14:37:08 +0900

    added tutorial

---
+++ src/components/TabTutorial.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-content">
+    <span
+      :class="{ 'q-nav': true, disabled: tutorialNumber <= 0 }"
+      @click="decrement"
+      >◀</span
+    >
+    Tutorial {{ tutorialNumber + 1 }}
+    <span
+      :class="{
+        'q-nav': true,
+        disabled: tutorialNumber >= tutorial.length - 1,
+      }"
+      @click="increment"
+      >▶</span
+    >
+    <div class="question-content">
+      <img :src="currentTutorial.img" /><br />
+      {{ currentTutorial.text }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import tutorial from "@/utils/tutorial";
+
+const tutorialNumber = ref(0);
+const currentTutorial = computed(() => tutorial[tutorialNumber.value]);
+function increment() {
+  if (tutorialNumber.value >= tutorial.length - 1) return;
+  tutorialNumber.value++;
+}
+function decrement() {
+  if (tutorialNumber.value <= 0) return;
+  tutorialNumber.value--;
+}
+</script>
+
+<style scoped>
+button {
+  background-color: white;
+  border: 1px solid #aaa;
+  padding: 10px;
+  border-radius: 10px;
+}
+.q-nav.disabled {
+  color: #ddd;
+}
+h2 {
+  margin: 0;
+}
+.question-content {
+  text-align: center;
+}
+img {
+  width: 100%;
+}
+</style>
---
+++ src/utils/tutorial.ts
@@ -0,0 +1,13 @@
+interface TutorialContent {
+  img: string;
+  text: string;
+}
+
+const tutorial: TutorialContent[] = [
+  {
+    img: "https://lh3.googleusercontent.com/pw/AL9nZEU8g7eBckM8WbQkjMY3X3mhZ1op3tNIYxerXQeGS2AUpT6_9QO-tGAxJT7KO7IkQE-8A_Wv8UqCowwJ3QfMwB4JAj_XRGDvUlR0J76fsZ5P0pfQCc51S05IYEd8jNSnjmCWjSSXxlCZ5SIbaUGWgj6EIA=w820-h418-no",
+    text: "Welcome to the tutorial!",
+  },
+];
+
+export default tutorial;
